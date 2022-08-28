22e021636f7709425a6b746fd5e8f165ce13ba3a
Ikramul Hasan <ikramhasan.dev@gmail.com>
2022-08-29 05:01:31 +0600

    added: colored states for insertion sort

--- lib/infrastructure/insertion_sort.dart
+++ lib/infrastructure/insertion_sort.dart
@@ -6,6 +6,7 @@ class InsertionSort implements SortingAlgorithm {
   Stream<List<Node>> sort(List<Node> array, Duration speed) async* {
     final newArray = [...array];
     int lengthOfArray = newArray.length;
+    newArray[0] = newArray[0].copyWith(state: NodeState.sorted);
 
     for (int j = 1; j < lengthOfArray; j++) {
       Node key = newArray[j];
@@ -13,8 +14,11 @@ class InsertionSort implements SortingAlgorithm {
       while (i >= 0 && newArray[i].value > key.value) {
         newArray[i + 1] = newArray[i];
         i = i - 1;
-        newArray[i + 1] = key;
+        newArray[i + 1] = key.copyWith(state: NodeState.checking);
+        await Future.delayed(speed);
+        yield newArray;
       }
+      newArray[i + 1] = newArray[i + 1].copyWith(state: NodeState.sorted);
       await Future.delayed(speed);
       yield newArray;
     }
